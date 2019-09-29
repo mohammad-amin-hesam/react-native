2d60107b538b2f135e6154a7fb6082123505fa53
Amin <singlepageprogrammer@gmail.com>
2019-09-29 10:53:27 +0330

    asdf

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -2,6 +2,11 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case "edit_blogpost":
+      return state.map(blogPost =>
+        blogPost.id === action.payload.id ? action.payload : blogPost
+      );
+
     case "delete_blogpost":
       return state.filter(blogPost => blogPost.id !== action.id);
 
@@ -25,6 +30,9 @@ const addBlogPost = dispatch => (title, content, callBack) => {
   callBack();
 };
 
+const editBlogPost = dispatch => (id, title, content) =>
+  dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+
 const deleteBlogPost = dispatch => id =>
   dispatch({ type: "delete_blogpost", id });
 
@@ -33,7 +41,8 @@ export const { Context, Provider } = createDataContext(
   {
     addBlogPost,
     deleteBlogPost,
-    addBlogPost
+    addBlogPost,
+    editBlogPost
   },
   [{ title: "Test Post", content: "test content", id: 29834719231273192 }]
 );
--- blog/src/screens/EditScreen.js
+++ blog/src/screens/EditScreen.js
@@ -3,16 +3,17 @@ import { Context } from "../context/BlogContext";
 import BlogPostForm from "../components/BlogPostFonts";
 
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context);
+  const { state, editBlogPost } = useContext(Context);
+  const id = navigation.getParam("id");
 
-  const blogPost = state.find(
-    blogPost => blogPost.id === navigation.getParam("id")
-  );
+  const blogPost = state.find(blogPost => blogPost.id === id);
 
   return (
     <BlogPostForm
       initialValues={{ title: blogPost.title, content: blogPost.content }}
-      onSubmit={(title, content) => {}}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content);
+      }}
     />
   );
 };
